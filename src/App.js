b7f531b9a85967173bf3dd3f71e51f07cfb912b2
Tristan Kerec <tkerec@uoguelph.ca>
2023-05-15 00:50:06 -0400

    font changes, added buttons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import React from 'react';
+import { isMobile } from 'react-device-detect';
+// import { ChevronDown } from 'bootstrap-icons';
+
 
 function App() {
 
@@ -12,26 +15,45 @@ function App() {
   return (
     <div className="App">
       <NavBar aboutRef={aboutRef} portfolioRef={portfolioRef} contactRef={contactRef}/>
-      <div style={{backgroundColor:'black', paddingTop: '30vh', color:'white' }}>
-        <h1 className="h1Text"  style={{fontWeight:'bold'}}>
+      <div style={{overflow:'hidden', backgroundColor:'black', paddingTop: '30vh', color:'white' }}>
+        <h1 className="h1Text"  style={{fontSize:h1Size(), fontWeight:'bold'}}>
           Tristan Kerec
         </h1>
-        <h2 className='h2Text' style={{fontWeight:'normal'}}>
+        <h2 className='h2Text' style={{fontSize:h1Size() * 0.5, fontWeight:'normal'}}>
           Bachelor of Computing Honours Graduate
           </h2>
-        <h3 className='h3Text' style={{fontWeight:'normal', paddingBottom:'100vh'}}>
+        <h3 className='h3Text' style={{ paddingBottom:'10px', fontSize:h1Size() * 0.43, fontWeight:'normal'}}>
           University of Guelph, Ontario, Canada
         </h3>
+        <button 
+            class="btn btn-link text-decoration-none"
+            type="button"
+            style={{fontWeight:'600', color:'#2ecc70'}}
+            onPress={() => aboutRef.current.scrollIntoView({behavior: "smooth", block: "start"})}>
+                Learn More <i class="bi bi-chevron-down"></i>
+        </button>
+        <a href="/2.png" download>
+  <button
+    className="btn btn-primary"
+    type="button"
+    style={{ fontWeight: '600', color: 'black', backgroundColor: '#2ecc70', borderColor: '#2ecc70' }}
+  >
+    Download Resume
+  </button>
+</a>
+
+        <div style={{paddingBottom:'100vh'}}></div>
         <div ref={aboutRef} style={{paddingBottom:'20vh'}}/>
-        <h1 className="h1Text"  style={{fontWeight:'bold', paddingBottom:'100vh'}}>
+        <h1 className="h1Text"  style={{fontSize:h1Size(), fontWeight:'bold', paddingBottom:'100vh'}}>
           About
         </h1>
+        
         <div ref={portfolioRef} style={{paddingBottom:'20vh'}}/>
-        <h1 className="h1Text" style={{fontWeight:'bold', paddingBottom:'100vh'}}>
+        <h1 className="h1Text" style={{fontSize:h1Size(), fontWeight:'bold', paddingBottom:'100vh'}}>
           Portfolio
         </h1>
         <div ref={contactRef} style={{paddingBottom:'20vh'}}/>
-        <h1 className="h1Text" style={{fontWeight:'bold', paddingBottom:'100vh'}}>
+        <h1 className="h1Text" style={{fontSize:h1Size(), fontWeight:'bold', paddingBottom:'100vh'}}>
           Contact
         </h1>
       </div>
@@ -55,3 +77,19 @@ function App() {
 }
 
 export default App;
+
+
+function h1Size() {
+  // if (Platform.OS === 'ios' || Platform.OS === 'android') {
+  //     if (Platform.isPad) {
+  //         return 20;
+  //     }
+  //     return 35;
+  // } else {
+    if (isMobile) {
+      return 35;
+    } else {
+      return 60;
+    }
+  // }
+}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
+import { isMobile } from 'react-device-detect';
+
 
 const NavBar = (props) => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const navbarStyles = {
+        top: 'env(safe-area-inset-top)', // Adjusts the top position for devices with rounded screens
+    };
+
+    const handleMenuToggle = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const handleMenuItemClick = () => {
+        setIsMenuOpen(false); // Close the menu when a menu item is clicked
+    };
+
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <nav style={navbarStyles} class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <div class="container-fluid">
-                <button class="btn btn-link text-decoration-none" type="button" style={{color:'white',fontWeight:'Bold'}} onClick={() => document.documentElement.scrollIntoView({behavior: "smooth", block: "start"})}>Tristan Kerec</button>
-                <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="btn btn-link text-decoration-none" type="button" style={{ fontSize: fontSize(), color: 'white', fontWeight: 'Bold' }} onClick={() => document.documentElement.scrollIntoView({ behavior: "smooth", block: "start" })}>Tristan Kerec</button>
+                <button class={`navbar-toggler ${isMenuOpen ? '' : 'collapsed'}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded={isMenuOpen} aria-label="Toggle navigation" onClick={handleMenuToggle}>
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="navbar-collapse collapse" id="navbarSupportedContent">
-                <ul class='navbar-nav d-flex ms-auto'>
-                    <li class="nav-item">
-                        <button class="btn btn-link text-decoration-none" style={{color:'white',fontWeight:'600'}} onClick={() => props.aboutRef.current.scrollIntoView({ behavior: 'smooth' })}>About</button>
-                    </li>
-                    <li class="nav-item">
-                        <button class="btn btn-link text-decoration-none" style={{color:'white',fontWeight:'600'}} onClick={() => props.portfolioRef.current.scrollIntoView({ behavior: 'smooth' })}>Portfolio</button>
-                    </li>
-                    <li class="nav-item">
-                        <button class="btn btn-link text-decoration-none" style={{color:'white',fontWeight:'600'}} onClick={() => props.contactRef.current.scrollIntoView({ behavior: 'smooth' })}>Contact</button>
-                    </li>
-                </ul>
+                <div class={`navbar-collapse collapse ${isMenuOpen ? 'show' : ''}`} id="navbarSupportedContent">
+                    <ul class='navbar-nav d-flex ms-auto'>
+                        <li class="nav-item">
+                            <button class="btn btn-link text-decoration-none" style={{ fontSize: fontSize(), color: 'white', fontWeight: '600' }} onClick={() => { props.aboutRef.current.scrollIntoView({ behavior: 'smooth' }); handleMenuItemClick(); }}>About</button>
+                        </li>
+                        <li class="nav-item">
+                            <button class="btn btn-link text-decoration-none" style={{ fontSize: fontSize(), color: 'white', fontWeight: '600' }} onClick={() => { props.portfolioRef.current.scrollIntoView({ behavior: 'smooth' }); handleMenuItemClick(); }}>Portfolio</button>
+                        </li>
+                        <li class="nav-item">
+                            <button class="btn btn-link text-decoration-none" style={{ fontSize: fontSize(), color: 'white', fontWeight: '600' }} onClick={() => { props.contactRef.current.scrollIntoView({ behavior: 'smooth' }); handleMenuItemClick(); }}>Contact</button>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
@@ -28,3 +44,11 @@ const NavBar = (props) => {
 };
 
 export default NavBar;
+
+function fontSize() {
+    if (isMobile) {
+        return 14;
+    } else {
+        return 20;
+    }
+}
