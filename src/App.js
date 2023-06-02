e39686018860c5fc6f143fd19a39dcde976678dd
Tristan Kerec <tkerec@uoguelph.ca>
2023-06-01 23:29:37 -0400

    finished contact section

--- src/App.js
+++ src/App.js
@@ -47,6 +47,7 @@ function App() {
           </button>
         </a>
         <div style={{display:'flex', justifyContent:'space-between',margin:'0 auto', maxWidth:'80vw'}}></div>
+        <a href="https://github.com/tristankerec" target='_blank' rel='noreferrer'>
           <button
           className="btn btn-link text-decoration-none"
           type="button"
@@ -55,14 +56,17 @@ function App() {
           style={{transition: 'color 0.3s', fontWeight:'500', color:'gray', fontSize:isMobile ? h1Size()*.6 : h1Size() * .38}}>
           <i className="bi bi-github"></i>
           </button>
+        </a>
+        <a href="https://www.linkedin.com/in/tristan-kerec-879a82198/" target='_blank' rel='noreferrer'>
           <button
           className="btn btn-link text-decoration-none"
           type="button"
-          onMouseEnter={(e) => {e.target.style.color = '#0077b5';}}
+          onMouseEnter={(e) => {e.target.style.color = '#0077b5'}}
           onMouseLeave={(e) => e.target.style.color = 'gray'}
           style={{transition: 'color 0.3s',fontWeight:'500', color:"gray", fontSize:isMobile ? h1Size()*.6 : h1Size() * .38}}>
           <i className="bi bi-linkedin"></i>
           </button>
+        </a>
         </div>
         <div style={{paddingBottom:'100vh'}}></div>
         <div ref={aboutRef} style={{paddingBottom:isMobile ? '12vh':'20vh'}}/>
@@ -72,11 +76,13 @@ function App() {
         <div style={{ textAlign:'center', maxWidth: isMobile ? '95vw' : '70vw', margin:'0 auto'}}>
           <p style={{fontSize:isMobile ? h1Size()*.45 : h1Size() * .32, fontWeight:'normal', paddingBottom:'1vh'}}>Hi! My name is Tristan Kerec, and I am a hard working, dedicated, and disciplined Bachelor of Computing Honours Graduate with a major in Software Engineering.</p>
           <p style={{fontSize:isMobile ? h1Size()*.45 : h1Size() * .32, fontWeight:'normal', paddingBottom:'1vh'}}>I build products that put the user experience first.</p>
-          <p style={{fontSize:isMobile ? h1Size()*.45 : h1Size() * .32, fontWeight:'normal', paddingBottom:'1vh'}}>Some of the programming languages, technologies, and frameworks that I am proficient in are JavaScript, React, Next.js, Vue.js, HTML, CSS, REST API, GraphQL, C, Python, Flutter, Java, Ruby, Rails, and SQL. All additional programming languages, technologies, and frameworks that I am proficient in can be found on my <a href="/TristanKerecCV.pdf" target="_blank" style={{color: '#2ecc70'}}>resume</a>.</p>
+          <p style={{fontSize:isMobile ? h1Size()*.45 : h1Size() * .32, fontWeight:'normal', paddingBottom:'1vh'}}>Some of the programming languages, technologies, and frameworks that I am proficient in are JavaScript, React, Next.js, Vue.js, HTML, CSS, REST API, GraphQL, C, Python, Flutter, Java, Ruby, Rails, and SQL. All additional programming languages, technologies, and frameworks that I am proficient in can be found on my <a href="/TristanKerecCV.pdf" target="_blank" onMouseEnter={(e) => e.target.style.color = '#1e9c53'} onMouseLeave={(e) => e.target.style.color = '#2ecc70'} style={{color: '#2ecc70'}}>resume</a>.</p>
           <p style={{fontSize:isMobile ? h1Size()*.45 : h1Size() * .32, fontWeight:'normal', paddingBottom:'0vh'}}>Continue to view some of my most exciting projects.</p>
           <button 
               className="btn btn-link text-decoration-none hover-button"
               type="button"
+              onMouseEnter={(e) => e.target.style.color = '#1e9c53'}
+              onMouseLeave={(e) => e.target.style.color = '#2ecc70'}
               style={{fontWeight:'500', color:'#2ecc70', fontSize:isMobile ? h1Size()*.9: h1Size() * .6}}
               onClick={() => portfolioRef.current.scrollIntoView({behavior: "smooth", block: "start"})}>
                 <i className="bi bi-chevron-down"></i>
--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -2,15 +2,36 @@ import { isMobile } from 'react-device-detect';
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import { serviceId, templateId, publicKey } from './Credentials.js';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify';
 
 const EmailForm = () => {
     const form = useRef();
 
     const sendEmail = (e) => {
         e.preventDefault();
+        const nameInput = form.current.querySelector('input[name="user_name"]');
+        const emailInput = form.current.querySelector('input[name="user_email"]');
+        const messageInput = form.current.querySelector('textarea[name="message"]');
+        if (!nameInput.value || !emailInput.value || !messageInput.value) {
+            toast.error('Please fill in all required fields.', {
+                position: "top-center",
+                autoClose: 3000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                });
+            return;
+        }
 
         emailjs.sendForm(serviceId, templateId, form.current, publicKey)
-        .then((result) => {
+        .then((result) => 
+        {
+            console.log(form.current);
             console.log(result.text);
         }, (error) => {
             console.log(error.text);
@@ -18,40 +39,58 @@ const EmailForm = () => {
     };
 
     return (
-        // <form ref={form} onSubmit={sendEmail}>
-        //     <label>Name</label>
-        //     <input type="text" name="user_name" />
-        //     <label>Email</label>
-        //     <input type="email" name="user_email" />
-        //     <label>Message</label>
-        //     <textarea name="message" />
-        //     <input type="submit" value="Send" />
-        // </form>
         <>
             <div style={{ paddingBottom:'1vh', display:'flex',justifyContent:'center', maxWidth:'80vw', margin: '0 auto'}}>
-                <h4 style={{fontSize: isMobile ? h1Size() * .5 : h1Size() * .32}}>Feel free to send me a quick message and I'll get back to you!</h4>
+                <h4 style={{fontSize: isMobile ? h1Size() * .5 : h1Size() * .32}}>Feel free to send me a message and I'll get back to you!</h4>
             </div>
             <form ref={form} onSubmit={sendEmail}>
                 <div style={{ textAlign: 'left', maxWidth: isMobile ? '88vw' : '60vw', margin: '0 auto' }}>
                     <div style={{ display: 'flex', justifyContent: 'space-between', paddingBottom:'0.5vh' }}>
                         <div style={{ width: '49.5%' }} className="form-group">
-                            <label style={{ paddingBottom:'0.2vh',fontSize: isMobile ? h1Size() * .45 : h1Size() * .32, fontWeight: '500' }} htmlFor="exampleInputName1">Name</label>
+                            <div style={{display: 'flex'}}>
+                                <label style={{ paddingBottom:'0.2vh',fontSize: isMobile ? h1Size() * .45 : h1Size() * .32, fontWeight: '500' }} htmlFor="exampleInputName1">Name</label>
+                                <h6 style={{color:'#FF6666'}}>*</h6>
+                            </div>
                             <input type="text" name="user_name" className="form-control" id="exampleInputName1" aria-describedby="nameHelp" placeholder="Enter name" />
                         </div>
                         <div style={{ width: '49.5%' }} className="form-group">
-                            <label style={{ paddingBottom:'0.2vh', fontSize: isMobile ? h1Size() * .45 : h1Size() * .32, fontWeight: '500' }} htmlFor="exampleInputEmail1">Email address</label>
+                            <div style={{display: 'flex'}}>
+                                <label style={{ paddingBottom:'0.2vh', fontSize: isMobile ? h1Size() * .45 : h1Size() * .32, fontWeight: '500' }} htmlFor="exampleInputEmail1">Email address</label>
+                                <h6 style={{color:'#FF6666'}}>*</h6>
+                            </div>
                             <input type="email" name="user_email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
                         </div>
                     </div>
                     <div className="form-group">
-                        <label style={{ paddingBottom:'0.2vh', fontSize: isMobile ? h1Size() * .45 : h1Size() * .32, fontWeight: '500' }}>Message</label>
+                        <div style={{display: 'flex'}}>
+                            <label style={{ paddingBottom:'0.2vh', fontSize: isMobile ? h1Size() * .45 : h1Size() * .32, fontWeight: '500' }}>Message</label>
+                            <h6 style={{color:'#FF6666'}}>*</h6>
+                        </div>
                         <textarea rows='3' name='message' type="text" className="form-control" placeholder="Enter a Message" />
                     </div>
                     <div style={{ paddingTop: '1.2vh', display: 'flex', justifyContent: 'end' }}>
-                        <button style={{ minWidth:'80px',fontWeight: '500', color: 'black', backgroundColor: '#2ecc70', borderColor: '#2ecc70' }} type="submit" className="btn btn-primary">Send</button>
+                        <button style={{ minWidth:'80px',fontWeight: '500', color: 'black', backgroundColor: '#2ecc70', borderColor: '#2ecc70' }} 
+                        type="submit" 
+                        className="btn btn-primary"
+                        onMouseEnter={(e) => {e.target.style.backgroundColor = '#1e9c53'; e.target.style.borderColor = '#1e9c53';}}
+                        onMouseLeave={(e) => {e.target.style.backgroundColor = '#2ecc70'; e.target.style.borderColor = '#2ecc70';}}>
+                        Send
+                        </button>
                     </div>
                 </div>
             </form>
+            <ToastContainer
+                position="top-center"
+                autoClose={3000}
+                hideProgressBar
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
         </>
     );
 };
